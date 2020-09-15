411763b0f44be3f7e47d03e8a8836d13f3e7aaa4
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-09-14 21:16:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+
+document.body.remove('#main')
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Charlie is the champion'
