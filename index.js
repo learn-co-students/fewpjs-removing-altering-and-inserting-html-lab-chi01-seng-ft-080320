8302bc28620d96a14c933523f856bc83eb5ebdcb
github username <github email address>
2020-09-18 15:02:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// this will remove the main with the ID of main/
+
+document.querySelector('main#main').remove();
+
+// creates a new header and we give it the ID of named victory
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+
+// we asssign a string for that new header and id
+newHeader.innerHTML = 'Josue is the champion'
